4a716e3b963023b8da242e62b01fd0ed035d7e29
Ajith Hussain <csy0013@googlemail.com>
2015-04-09 09:17:47 +1000

    New API

--- main.go
+++ main.go
@@ -1,95 +1,92 @@
 package main
 
 import (
-	"fmt"
 	"io"
 	"net/http"
-	"os"
 
+	"github.com/kr/pretty"
 	"github.com/sparkymat/resty"
 )
 
 type GroupsController struct {
 }
 
-func (controller GroupsController) Index(response http.ResponseWriter, request *http.Request, params map[string]string) {
-	io.WriteString(response, "groups#index/n")
+func (controller GroupsController) Index(response http.ResponseWriter, request *http.Request, params map[string][]string) {
+	io.WriteString(response, "groups#index\n")
 	for key, value := range params {
-		io.WriteString(response, fmt.Sprintf("%v = %v\n", key, value))
+		io.WriteString(response, pretty.Sprintf("%v = %v\n", key, value))
 	}
 }
-func (controller GroupsController) Create(response http.ResponseWriter, request *http.Request, params map[string]string) {
-	io.WriteString(response, "groups#create/n")
+func (controller GroupsController) Create(response http.ResponseWriter, request *http.Request, params map[string][]string) {
+	io.WriteString(response, "groups#create\n")
 	for key, value := range request.Form {
-		io.WriteString(response, fmt.Sprintf("%v = %v\n", key, value))
+		io.WriteString(response, pretty.Sprintf("%v = %v\n", key, value))
 	}
 }
-func (controller GroupsController) Update(response http.ResponseWriter, request *http.Request, params map[string]string) {
-	io.WriteString(response, "groups#update/n")
+func (controller GroupsController) Update(response http.ResponseWriter, request *http.Request, params map[string][]string) {
+	io.WriteString(response, "groups#update\n")
 	for key, value := range params {
-		io.WriteString(response, fmt.Sprintf("%v = %v\n", key, value))
+		io.WriteString(response, pretty.Sprintf("%v = %v\n", key, value))
 	}
 }
-func (controller GroupsController) Destroy(response http.ResponseWriter, request *http.Request, params map[string]string) {
-	io.WriteString(response, "groups#destroy/n")
+func (controller GroupsController) Destroy(response http.ResponseWriter, request *http.Request, params map[string][]string) {
+	io.WriteString(response, "groups#destroy\n")
 	for key, value := range params {
-		io.WriteString(response, fmt.Sprintf("%v = %v\n", key, value))
+		io.WriteString(response, pretty.Sprintf("%v = %v\n", key, value))
 	}
 }
 
-func (controller GroupsController) Show(response http.ResponseWriter, request *http.Request, params map[string]string) {
+func (controller GroupsController) Show(response http.ResponseWriter, request *http.Request, params map[string][]string) {
 	io.WriteString(response, "groups#show\n")
 	for key, value := range params {
-		io.WriteString(response, fmt.Sprintf("%v = %v", key, value))
+		io.WriteString(response, pretty.Sprintf("%v = %v", key, value))
 	}
 }
 
 type GroupMembershipsController struct {
 }
 
-func (controller GroupMembershipsController) Index(response http.ResponseWriter, request *http.Request, params map[string]string) {
+func (controller GroupMembershipsController) Index(response http.ResponseWriter, request *http.Request, params map[string][]string) {
 	io.WriteString(response, "group_memberships#index\n")
 	for key, value := range params {
-		io.WriteString(response, fmt.Sprintf("%v = %v", key, value))
+		io.WriteString(response, pretty.Sprintf("%v = %v", key, value))
 	}
 }
 
-func (controller GroupMembershipsController) Create(response http.ResponseWriter, request *http.Request, params map[string]string) {
+func (controller GroupMembershipsController) Create(response http.ResponseWriter, request *http.Request, params map[string][]string) {
 	io.WriteString(response, "group_memberships#create\n")
 	for key, value := range params {
-		io.WriteString(response, fmt.Sprintf("%v = %v", key, value))
+		io.WriteString(response, pretty.Sprintf("%v = %v", key, value))
 	}
 }
 
-func (controller GroupMembershipsController) Update(response http.ResponseWriter, request *http.Request, params map[string]string) {
+func (controller GroupMembershipsController) Update(response http.ResponseWriter, request *http.Request, params map[string][]string) {
 	io.WriteString(response, "group_memberships#update\n")
 	for key, value := range params {
-		io.WriteString(response, fmt.Sprintf("%v = %v", key, value))
+		io.WriteString(response, pretty.Sprintf("%v = %v", key, value))
 	}
 }
 
-func (controller GroupMembershipsController) Destroy(response http.ResponseWriter, request *http.Request, params map[string]string) {
+func (controller GroupMembershipsController) Destroy(response http.ResponseWriter, request *http.Request, params map[string][]string) {
 	io.WriteString(response, "group_memberships#destroy\n")
 	for key, value := range params {
-		io.WriteString(response, fmt.Sprintf("%v = %v", key, value))
+		io.WriteString(response, pretty.Sprintf("%v = %v", key, value))
 	}
 }
 
-func (controller GroupMembershipsController) Show(response http.ResponseWriter, request *http.Request, params map[string]string) {
+func (controller GroupMembershipsController) Show(response http.ResponseWriter, request *http.Request, params map[string][]string) {
 	io.WriteString(response, "group_memberships#show\n")
 	for key, value := range params {
-		io.WriteString(response, fmt.Sprintf("%v = %v", key, value))
+		io.WriteString(response, pretty.Sprintf("%v = %v", key, value))
 	}
 }
 
 func main() {
-	groupHandler := resty.Resource("groups").Controller(GroupsController{})
-	groupMembershipHandler := resty.Resource("groups", "group_memberships").Controller(GroupMembershipsController{})
+	r := resty.ResourceRouter{}
+	r.Resource([]string{"groups"}, GroupsController{})
+	r.Resource([]string{"groups", "group_memberships"}, GroupMembershipsController{})
 
-	groupMembershipHandler.HandleRoot()
+	r.HandleRoot()
 
-	groupMembershipHandler.PrintRoutes(os.Stdout)
-	groupHandler.PrintRoutes(os.Stdout)
-
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":5000", nil)
 }
